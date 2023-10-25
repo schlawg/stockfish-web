2fec28a1320f91dedc7acd2693b7fb6cdf20085f
Jonathan Gamble <gamblej@gmail.com>
2023-10-25 05:04:40 -0500

    0.1.0

---
+++ scripts/pre-publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S bash -e
+
+cd "$(dirname "${BASH_SOURCE:-$0}")/.."
+
+if [[ $# -gt 0 ]]; then
+  shift
+  ARGS="OPT=\"$@\""
+fi
+
+git checkout smollnet-16ish
+
+eval "make clean && make -j $ARGS"
+mv stockfishWeb.wasm stockfishWeb.7.wasm
+
+git checkout sf-16
+
+eval "make clean && make -j $ARGS"
